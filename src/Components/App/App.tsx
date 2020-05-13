40487b1064660c0cede33adf0c1e0133cb9fbb5b
Karen Garcia <kgarcia@i3screen.com>
2020-05-13 07:38:10 -0600

    Got delete button to work

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,34 +1,156 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Styling/App.scss';
 import TestData from './TestData';
-
+import TestDataProps from './TestData';
 
 function App() {
+  const [data, setData] = useState([
+    {
+      monthId: 1,
+      monthName: 'January',
+      begBalance: 1000,
+      endBalance: 100,
+      monthData: [{ title: 'HOA', cost: 204, monthIN: 1 }],
+    },
+    {
+      monthId: 2,
+      monthName: 'February',
+      begBalance: 100,
+      endBalance: 100,
+      monthData: [{ title: 'Property Taxes', cost: 1275.89, monthIN: 2 }],
+    },
+    {
+      monthId: 3,
+      monthName: 'March',
+      begBalance: 100,
+      endBalance: -100,
+      monthData: [],
+    },
+    {
+      monthId: 4,
+      monthName: 'April',
+      begBalance: 1000,
+      endBalance: 100,
+      monthData: [
+        { title: 'Income Taxes', cost: 4000, monthIN: 4 },
+        { title: 'Home Insurance', cost: 865.73, monthIN: 4 },
+        { title: 'Auto Insurance', cost: 670.87, monthIN: 4 },
+      ],
+    },
+    {
+      monthId: 5,
+      monthName: 'May',
+      begBalance: 1000,
+      endBalance: 100,
+      monthData: [],
+    },
+    {
+      monthId: 6,
+      monthName: 'June',
+      begBalance: 1000,
+      endBalance: 100,
+      monthData: [{ title: 'Property Taxes', cost: 1275.89, monthIN: 6 }],
+    },
+    {
+      monthId: 7,
+      monthName: 'July',
+      begBalance: 1000,
+      endBalance: 100,
+      monthData: [
+        { title: 'HOA Dues', cost: 189.5, monthIN: 7 },
+        { title: 'Amazon', cost: 120, monthIN: 7 },
+      ],
+    },
+    {
+      monthId: 8,
+      monthName: 'August',
+      begBalance: 1000,
+      endBalance: 100,
+      monthData: [{ title: 'Life Insurance', cost: 670.87, monthIN: 8 }],
+    },
+    {
+      monthId: 9,
+      monthName: 'September',
+      begBalance: 1000,
+      endBalance: 100,
+      monthData: [],
+    },
+    {
+      monthId: 10,
+      monthName: 'October',
+      begBalance: 1000,
+      endBalance: 100,
+      monthData: [],
+    },
+    {
+      monthId: 11,
+      monthName: 'November',
+      begBalance: 1000,
+      endBalance: 100,
+      monthData: [{ title: 'Costco', cost: 120, monthIN: 11 }],
+    },
+    {
+      monthId: 12,
+      monthName: 'December',
+      begBalance: 1000,
+      endBalance: 100,
+      monthData: [{ title: 'Vehicle Registration', cost: 240.55, monthIN: 12 }],
+    },
+  ]);
 
-    return (
-        <div className="App">
-            <header className="App-header">
-                Less Frequent Expenses (Happens less often than monthly) Items you cannot schedule monthly in your budget
-            </header>
-            {TestData.map((data, index)=>
-                <section  key={index} className={(data.endBalance >=0) ? 'monthCard' : 'monthCard negativeMonthCard'}>
-                    <h2>Month: {data.monthName}</h2>
-                    <div className={(data.begBalance >=0) ? 'positiveBalance' : 'negativeBalance'}>
-                        Beginning Balance: ${data.begBalance}
-                    </div>
-                    {data.monthData.map((data,index)=>
-                        <div key={index}>
-                            {data.title} ${data.cost}
-                            <button onClick={()=>console.log('delete')}>X</button>
-                        </div>
-                    )}
-                <div className={(data.endBalance >=0) ? 'positiveBalance' : 'negativeBalance'}>
-                    Ending Balance: ${data.endBalance}
-                </div>
-                </section>)
+  interface handleUpdateProps {
+    month: number;
+    index: number;
+  }
+  const handleUpdate = (month: number, index: number) => {
+    let initialData = [...data];
+    // data[month-1].monthData.splice(index,1);
+      initialData[month-1].monthData.splice(index,1);
+    console.log({ initialData });
+    setData(initialData);
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        Less Frequent Expenses (Happens less often than monthly) Items you
+        cannot schedule monthly in your budget
+      </header>
+      {data.map((data, index) => (
+        <section
+          key={index}
+          className={
+            data.endBalance >= 0 ? 'monthCard' : 'monthCard negativeMonthCard'
+          }
+        >
+          <h2>Month: {data.monthName}</h2>
+          <div
+            className={
+              data.begBalance >= 0 ? 'positiveBalance' : 'negativeBalance'
             }
-        </div>
-    );
+          >
+            Beginning Balance: ${data.begBalance}
+          </div>
+          {data.monthData.map((data, index) => (
+            <div key={index}>
+              {data.title} ${data.cost}
+              {data.monthIN && (
+                <button onClick={() => handleUpdate(data.monthIN, index)}>
+                  X
+                </button>
+              )}
+            </div>
+          ))}
+          <div
+            className={
+              data.endBalance >= 0 ? 'positiveBalance' : 'negativeBalance'
+            }
+          >
+            Ending Balance: ${data.endBalance}
+          </div>
+        </section>
+      ))}
+    </div>
+  );
 }
 
 export default App;
--- src/Components/App/TestData.tsx
+++ src/Components/App/TestData.tsx
@@ -1,5 +1,5 @@
 
-interface TestDataProps {
+export interface TestDataProps {
     monthId: number,
     monthName: string,
     begBalance: number,
@@ -16,26 +16,21 @@ const TestData =
             monthName: 'January',
             begBalance: 1000,
             endBalance: 100,
-            monthData: [
-                {title: "HOA", cost: 204},
-            ]
+            monthData: [{ title: 'HOA', cost: 204, monthIN: 1 }],
         },
         {
             monthId: 2,
             monthName: 'February',
             begBalance: 100,
             endBalance: 100,
-            monthData: [
-                {title: "Property Taxes", cost: 1275.89},
-            ]
+            monthData: [{ title: 'Property Taxes', cost: 1275.89, monthIN: 2 }],
         },
         {
             monthId: 3,
             monthName: 'March',
             begBalance: 100,
             endBalance: -100,
-            monthData: [
-            ]
+            monthData: [],
         },
         {
             monthId: 4,
@@ -43,27 +38,24 @@ const TestData =
             begBalance: 1000,
             endBalance: 100,
             monthData: [
-                {title: "Income Taxes", cost: 4000},
-                {title: "Home Insurance", cost: 865.73},
-                {title: "Auto Insurance", cost: 670.87},
-            ]
+                { title: 'Income Taxes', cost: 4000, monthIN: 4 },
+                { title: 'Home Insurance', cost: 865.73, monthIN: 4 },
+                { title: 'Auto Insurance', cost: 670.87, monthIN: 4 },
+            ],
         },
         {
             monthId: 5,
             monthName: 'May',
             begBalance: 1000,
             endBalance: 100,
-            monthData: [
-            ]
+            monthData: [],
         },
         {
             monthId: 6,
             monthName: 'June',
             begBalance: 1000,
             endBalance: 100,
-            monthData: [
-                {title: "Property Taxes", cost: 1275.89},
-            ]
+            monthData: [{ title: 'Property Taxes', cost: 1275.89, monthIN: 6 }],
         },
         {
             monthId: 7,
@@ -71,52 +63,44 @@ const TestData =
             begBalance: 1000,
             endBalance: 100,
             monthData: [
-                {title: "HOA Dues", cost: 189.50},
-                {title: "Amazon", cost: 120},
-            ]
+                { title: 'HOA Dues', cost: 189.5, monthIN: 7 },
+                { title: 'Amazon', cost: 120, monthIN: 7 },
+            ],
         },
         {
             monthId: 8,
             monthName: 'August',
             begBalance: 1000,
             endBalance: 100,
-            monthData: [
-                {title: "Life Insurance", cost: 670.87},
-            ]
+            monthData: [{ title: 'Life Insurance', cost: 670.87, monthIN: 8 }],
         },
         {
             monthId: 9,
             monthName: 'September',
             begBalance: 1000,
             endBalance: 100,
-            monthData: [
-            ]
+            monthData: [],
         },
         {
             monthId: 10,
             monthName: 'October',
             begBalance: 1000,
             endBalance: 100,
-            monthData: [
-            ]
+            monthData: [],
         },
         {
             monthId: 11,
             monthName: 'November',
             begBalance: 1000,
             endBalance: 100,
-            monthData: [
-                {title: "Costco", cost: 120},
-            ]
+            monthData: [{ title: 'Costco', cost: 120, monthIN: 11 }],
         },
         {
             monthId: 12,
             monthName: 'December',
             begBalance: 1000,
             endBalance: 100,
-            monthData: [
-                {title: "Vehicle Registration", cost: 240.55},
-            ]
+            monthData: [{ title: 'Vehicle Registration', cost: 240.55, monthIN: 12 }],
         },
     ];
 
