03e552e6dd010b9bbace9151088e25be4f81ae30
Karen Garcia <kgarcia@i3screen.com>
2020-05-14 20:57:44 -0600

    Updated initial data Updated the initial data to look more realistic to start with so now I can work on the enhancements of adding items and removing them to make sure everything is working properly

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -8,29 +8,29 @@ function App() {
     {
       monthId: 1,
       monthName: 'January',
-      begBalance: 1000,
-      endBalance: 100,
+      begBalance: 4000,
+      endBalance: 4496,
       monthData: [{ title: 'HOA', cost: 204, monthIN: 1 }],
     },
     {
       monthId: 2,
       monthName: 'February',
-      begBalance: 100,
-      endBalance: 100,
+      begBalance: 4496,
+      endBalance: 3920.11,
       monthData: [{ title: 'Property Taxes', cost: 1275.89, monthIN: 2 }],
     },
     {
       monthId: 3,
       monthName: 'March',
-      begBalance: 100,
-      endBalance: 100,
+      begBalance: 3920.11,
+      endBalance: 4620.11,
       monthData: [],
     },
     {
       monthId: 4,
       monthName: 'April',
-      begBalance: 1000,
-      endBalance: 100,
+      begBalance: 4620.11,
+      endBalance: -216.49,
       monthData: [
         { title: 'Income Taxes', cost: 4000, monthIN: 4 },
         { title: 'Home Insurance', cost: 865.73, monthIN: 4 },
@@ -40,22 +40,22 @@ function App() {
     {
       monthId: 5,
       monthName: 'May',
-      begBalance: 1000,
-      endBalance: 100,
+      begBalance: -216.49,
+      endBalance: 483.51,
       monthData: [],
     },
     {
       monthId: 6,
       monthName: 'June',
-      begBalance: 1000,
-      endBalance: 100,
+      begBalance: 483.51,
+      endBalance: -92.38,
       monthData: [{ title: 'Property Taxes', cost: 1275.89, monthIN: 6 }],
     },
     {
       monthId: 7,
       monthName: 'July',
-      begBalance: 1000,
-      endBalance: 100,
+      begBalance: -92.38,
+      endBalance: 298.12,
       monthData: [
         { title: 'HOA Dues', cost: 189.5, monthIN: 7 },
         { title: 'Amazon', cost: 120, monthIN: 7 },
@@ -64,47 +64,47 @@ function App() {
     {
       monthId: 8,
       monthName: 'August',
-      begBalance: 1000,
-      endBalance: 100,
+      begBalance: 298.12,
+      endBalance: 327.25,
       monthData: [{ title: 'Life Insurance', cost: 670.87, monthIN: 8 }],
     },
     {
       monthId: 9,
       monthName: 'September',
-      begBalance: 1000,
-      endBalance: 100,
+      begBalance: 327.25,
+      endBalance: 1027.25,
       monthData: [],
     },
     {
       monthId: 10,
       monthName: 'October',
-      begBalance: 1000,
-      endBalance: 100,
+      begBalance: 1027.25,
+      endBalance: 1727.25,
       monthData: [],
     },
     {
       monthId: 11,
       monthName: 'November',
-      begBalance: 1000,
-      endBalance: 100,
+      begBalance: 1727.25,
+      endBalance: 2307.25,
       monthData: [{ title: 'Costco', cost: 120, monthIN: 11 }],
     },
     {
       monthId: 12,
       monthName: 'December',
-      begBalance: 1000,
-      endBalance: 100,
+      begBalance: 2307.25,
+      endBalance: 2766.70,
       monthData: [{ title: 'Vehicle Registration', cost: 240.55, monthIN: 12 }],
     },
   ]);
 
   //Input section
-  const [initialFunding, setInitialFunding] = useState(1000);
+  const [initialFunding, setInitialFunding] = useState(4000);
   const [costTitle, setCostTitle] = useState<string>('');
   const [costAmount, setCostAmount] = useState<number>(0);
 
   const [optionsState, setOptionsState] = useState<string>();
-  const [monthlyContribution, setMonthlyContribution] = useState<number>(100);
+  const [monthlyContribution, setMonthlyContribution] = useState<number>(700);
 
   const [menu, setMenu] = useState(true);
   interface handleUpdateProps {
@@ -124,11 +124,11 @@ function App() {
     }
   };
 
-  const handleAddition = () => {
+  const handleAddition = (costTitle: string, costAmount: number, optionsState: number) => {
     let object = [...data];
     //add to december index is number-1
-    object[11].monthData.push({ title: 'Testing', cost: 777.0, monthIN: 12 });
-
+    object[optionsState-1].monthData.push({ title: costTitle, cost: costAmount, monthIN: optionsState });
+   console.log({costTitle, costAmount, optionsState});
     handleUpdateDataState();
   };
 
@@ -236,25 +236,25 @@ function App() {
                   required
                   className="inputBox"
                 >
-                  <option value="January">SELECT ONE</option>
-                  <option value="January">January</option>
-                  <option value="February">February</option>
-                  <option value="March">March</option>
-                  <option value="April">April</option>
-                  <option value="May">May</option>
-                  <option value="June">June</option>
-                  <option value="July">July</option>
-                  <option value="August">August</option>
-                  <option value="September">September</option>
-                  <option value="October">October</option>
-                  <option value="November">November</option>
-                  <option value="December">December</option>
+                  <option value={0}>SELECT ONE</option>
+                  <option value={1}>January</option>
+                  <option value={2}>February</option>
+                  <option value={3}>March</option>
+                  <option value={4}>April</option>
+                  <option value={5}>May</option>
+                  <option value={6}>June</option>
+                  <option value={7}>July</option>
+                  <option value={8}>August</option>
+                  <option value={9}>September</option>
+                  <option value={10}>October</option>
+                  <option value={11}>November</option>
+                  <option value={12}>December</option>
                 </select>
                 {costTitle}${costAmount}
                 {optionsState}
-                <div className="submit" onClick={() => handleAddition()}>
+                {(optionsState !== undefined) && <div className="submit" onClick={() => handleAddition(costTitle, costAmount, parseInt(optionsState))}>
                   Submit
-                </div>
+                </div>}
               </label>
             </div>
           </section>
