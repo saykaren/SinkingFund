d1867539bf258ac01603f778b998366ea4003f8f
Karen Garcia <kgarcia@i3screen.com>
2020-05-16 22:43:59 -0600

    Updates

---
+++ precache-manifest.b8a186725050784862cff787c37015a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf2ca7dd7ede8a92048fc5ffbfd88cd8",
+    "url": "/SinkingFund/index.html"
+  },
+  {
+    "revision": "cc35f2d2a5552b6f7923",
+    "url": "/SinkingFund/static/css/main.858009c9.chunk.css"
+  },
+  {
+    "revision": "6a014793f1163e600577",
+    "url": "/SinkingFund/static/js/2.76f609fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SinkingFund/static/js/2.76f609fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc35f2d2a5552b6f7923",
+    "url": "/SinkingFund/static/js/main.85ed1014.chunk.js"
+  },
+  {
+    "revision": "d1eaf79bf215e8f3fa49",
+    "url": "/SinkingFund/static/js/runtime-main.577b2e71.js"
+  },
+  {
+    "revision": "d0827d01ac17b128172a9f867b8d0c82",
+    "url": "/SinkingFund/static/media/sayKaren_logo-white_8.1.2019.d0827d01.png"
+  }
+]);
