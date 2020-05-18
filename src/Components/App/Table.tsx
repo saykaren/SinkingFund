c8bdc39159bff343795353a41871a59a626638c3
Karen Garcia <kgarcia@i3screen.com>
2020-05-18 15:39:35 -0600

    Added html table of data
    Added html table of data for client to be able to easily copy and store expenses

--- src/Components/App/Table.tsx
+++ src/Components/App/Table.tsx
@@ -5,13 +5,39 @@ import dataProps from "./Interface";
 const Table = ({ data }: dataProps) => {
   return (
     <>
-        {data && <div>
-            {data.map((num,index)=>(
-                <div key={index}>
-                    {num.monthName}
-                </div>
-            ))}
-        </div>}
+      <table id="table-to-xls">
+        <thead>
+        <tr>
+          <th>Expense Title</th>
+          <th>Expense Cost</th>
+          <th>Expense Month</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+          {data && data.map((num, index)=>(
+                num.monthData && num.monthData.map((exp, index)=>(
+                     <tr>
+                      <td key={index}>{exp.title}
+                      </td>
+                       <td key={index}>{exp.cost}
+                       </td>
+                       <td key={index}>{exp.monthIN}
+                       </td>
+                    </tr>
+                  ))
+              ))}
+        </tr>
+        </tbody>
+      </table>
+        {/*{data && <div>*/}
+        {/*    {data.map((num,index)=>(*/}
+        {/*        <div key={index}>*/}
+        {/*            {num.monthName}*/}
+
+        {/*        </div>*/}
+        {/*    ))}*/}
+        {/*</div>}*/}
     </>
   );
 };
